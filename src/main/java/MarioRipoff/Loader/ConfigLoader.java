a25cd172a2136592a3ce7a9b8200ac32e08acc5b
felixrmann <felixmann00@gmal.com>
2021-06-22 13:38:51 +0200

    added a ConfigLoader and a PlayerLoader to load data from the json files

--- src/main/java/MarioRipoff/Loader/ConfigLoader.java
+++ src/main/java/MarioRipoff/Loader/ConfigLoader.java
@@ -1,5 +1,13 @@
 package MarioRipoff.Loader;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
 /**
  * @author Felix Mann
  * @version 1.0
@@ -7,5 +15,29 @@ package MarioRipoff.Loader;
  */
 
 public class ConfigLoader {
-    
+
+    private static String fileName = "config.json";
+    private static final String filePath = "src\\main\\resources\\Config\\";
+
+    /**
+     * returns the requested config data
+     * @return the config data
+     */
+    public static JSONObject loadPlayer(){
+        return new JSONObject(Objects.requireNonNull(loadFileString()));
+    }
+
+    /**
+     load file content to String
+     @return String
+     */
+    private static String loadFileString(){
+        try {
+            return Files.readString(Path.of(filePath + fileName), StandardCharsets.US_ASCII);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
--- src/main/java/MarioRipoff/Loader/PlayerLoader.java
+++ src/main/java/MarioRipoff/Loader/PlayerLoader.java
@@ -1,5 +1,13 @@
 package MarioRipoff.Loader;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
 /**
  * @author Felix Mann
  * @version 1.0
@@ -8,4 +16,30 @@ package MarioRipoff.Loader;
 
 public class PlayerLoader {
 
+    private static String fileName = "";
+    private static final String filePath = "src\\main\\resources\\Player\\";
+
+    /**
+     * returns the requested playerData
+     * @param playerName
+     * @return the playerData
+     */
+    public static JSONObject loadPlayer(String playerName){
+        PlayerLoader.fileName = playerName + ".json";
+
+        return new JSONObject(Objects.requireNonNull(loadFileString()));
+    }
+
+    /**
+     load file content to String
+     @return String
+     */
+    private static String loadFileString(){
+        try {
+            return Files.readString(Path.of(filePath + fileName), StandardCharsets.US_ASCII);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
